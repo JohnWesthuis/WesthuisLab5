57153aead8074297218317104656cb349b48b7c0
John Westhuis <johnwwesthuis@outlook.com>
2018-02-19 18:58:51 -0500

    Initialize

---
+++ Lab5/Data/Lab5Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5.Models
+{
+    public class Lab5Context : DbContext
+    {
+        public Lab5Context (DbContextOptions<Lab5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab5.Models.Movie> Movie { get; set; }
+    }
+}
